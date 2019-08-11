156f87bc3643199ea9e59f60666955ec07c172e2
marin <marin.radjenovic@alite-international.com>
2019-08-12 01:04:53 +0200

    bah

---
+++ infrastructure/myexample.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-8803e0f0"
+  instance_type = "t2.micro"
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the web server"
+}
